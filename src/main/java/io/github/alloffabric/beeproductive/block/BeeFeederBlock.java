a245c44a0f071eba662f1a9b35f0c7710127f9f3
Meredith Espinosa <goclonefilms@gmail.com>
2019-12-15 22:24:06 -0800

    add most of the recipes, set up feeder block

--- src/main/java/io/github/alloffabric/beeproductive/block/BeeFeederBlock.java
+++ src/main/java/io/github/alloffabric/beeproductive/block/BeeFeederBlock.java
@@ -1,18 +1,78 @@
 package io.github.alloffabric.beeproductive.block;
 
 import io.github.alloffabric.beeproductive.api.Nectar;
+import io.github.alloffabric.beeproductive.block.entity.BeeFeederBlockEntity;
 import io.github.alloffabric.beeproductive.init.BeeProdNectars;
+import io.github.alloffabric.beeproductive.item.NectarItem;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.InventoryProvider;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SidedInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
-public class BeeFeederBlock extends Block {
+import javax.annotation.Nullable;
+
+public class BeeFeederBlock extends Block implements InventoryProvider, BlockEntityProvider {
 	public BeeFeederBlock(Settings settings) {
 		super(settings);
 	}
 
+	@Override
+	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+		if (world.isClient) return ActionResult.SUCCESS;
+		BlockEntity be = world.getBlockEntity(pos);
+		if (be instanceof Inventory) {
+			Inventory in = (Inventory)be;
+			ItemStack stack = player.getStackInHand(hand);
+			if (stack.getItem() instanceof NectarItem || stack.isEmpty()) {
+				ItemStack invStack = in.getInvStack(0);
+				in.setInvStack(0, stack);
+				player.setStackInHand(hand, invStack);
+				player.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 1F, 1F);
+				return ActionResult.SUCCESS;
+			}
+		}
+		return ActionResult.SUCCESS;
+	}
+
 	public Nectar consumeNectar(World world, BlockPos pos) {
-		//TODO: impl
+		BlockEntity be = world.getBlockEntity(pos);
+		if (be instanceof Inventory) {
+			Inventory in = (Inventory)be;
+			ItemStack stack = in.getInvStack(0);
+			if (stack.getItem() instanceof NectarItem) {
+				Nectar nectar = ((NectarItem)stack.getItem()).getNectar();
+				stack.decrement(1);
+				return nectar;
+			}
+		}
 		return BeeProdNectars.EMPTY;
+	}
+
+	@Override
+	public SidedInventory getInventory(BlockState state, IWorld world, BlockPos pos) {
+		BlockEntity be = world.getBlockEntity(pos);
+		if (be instanceof SidedInventory) {
+			return (SidedInventory)be;
+		}
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public BlockEntity createBlockEntity(BlockView view) {
+		return new BeeFeederBlockEntity();
 	}
 }
--- src/main/java/io/github/alloffabric/beeproductive/block/entity/BeeFeederBlockEntity.java
+++ src/main/java/io/github/alloffabric/beeproductive/block/entity/BeeFeederBlockEntity.java
@@ -1,10 +1,75 @@
 package io.github.alloffabric.beeproductive.block.entity;
 
 import io.github.alloffabric.beeproductive.init.BeeProdBlockEntities;
+import io.github.alloffabric.beeproductive.item.NectarItem;
+import io.github.alloffabric.beeproductive.util.ImplementedInventory;
+import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.SidedInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.DefaultedList;
+import net.minecraft.util.math.Direction;
 
-public class BeeFeederBlockEntity extends BlockEntity {
+import javax.annotation.Nullable;
+
+public class BeeFeederBlockEntity extends BlockEntity implements ImplementedInventory, SidedInventory, BlockEntityClientSerializable {
 	public BeeFeederBlockEntity() {
 		super(BeeProdBlockEntities.FEEDER_ENTITY);
+	}
+	private DefaultedList<ItemStack> items = DefaultedList.ofSize(1, ItemStack.EMPTY);
+
+	@Override
+	public DefaultedList<ItemStack> getItems() {
+		return items;
+	}
+
+	@Override
+	public void markDirty() {
+		sync();
+	}
+
+	@Override
+	public void fromTag(CompoundTag tag) {
+		super.fromTag(tag);
+		Inventories.fromTag(tag.getCompound("Inventory"), items);
+	}
+
+	@Override
+	public CompoundTag toTag(CompoundTag tag) {
+		tag.put("Inventory", Inventories.toTag(new CompoundTag(), items));
+		return super.toTag(tag);
+	}
+
+	@Override
+	public void fromClientTag(CompoundTag tag) {
+		fromTag(tag);
+	}
+
+	@Override
+	public CompoundTag toClientTag(CompoundTag tag) {
+		return toTag(tag);
+	}
+
+	@Override
+	public int[] getInvAvailableSlots(Direction side) {
+		// Just return an array of all slots
+		int[] result = new int[getItems().size()];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = i;
+		}
+
+		return result;
+	}
+
+	@Override
+	public boolean canInsertInvStack(int slot, ItemStack stack, @Nullable Direction dir) {
+		return stack.getItem() instanceof NectarItem;
+	}
+
+	@Override
+	public boolean canExtractInvStack(int slot, ItemStack stack, Direction dir) {
+		return true;
 	}
 }
