ca9d64b34d7101e8fea35208fc2811281d5edcbf
Meredith Espinosa <goclonefilms@gmail.com>
2019-12-17 12:19:08 -0800

    update to newer bee comaptible

--- src/main/java/io/github/alloffabric/beeproductive/init/BeeProdEvents.java
+++ src/main/java/io/github/alloffabric/beeproductive/init/BeeProdEvents.java
@@ -4,18 +4,21 @@ import io.github.alloffabric.beeproductive.BeeProductive;
 import io.github.alloffabric.beeproductive.api.BeeComponent;
 import io.github.cottonmc.beecompatible.api.BeeTimeCheckCallback;
 import io.github.cottonmc.beecompatible.api.BeeWeatherCheckCallback;
+import net.fabricmc.fabric.api.util.TriState;
 
 public class BeeProdEvents {
 
 	public static void init() {
 		BeeTimeCheckCallback.EVENT.register((world, bee) -> {
 			BeeComponent comp = BeeProductive.BEE_COMPONENT.get(bee);
-			return comp.getTraitValue(BeeProdTraits.NOCTURNAL);
+			if (comp.getTraitValue(BeeProdTraits.NOCTURNAL)) return TriState.TRUE;
+			return TriState.DEFAULT;
 		});
 
 		BeeWeatherCheckCallback.EVENT.register((world, bee) -> {
 			BeeComponent comp = BeeProductive.BEE_COMPONENT.get(bee);
-			return comp.getTraitValue(BeeProdTraits.WEATHERPROOF);
+			if (comp.getTraitValue(BeeProdTraits.WEATHERPROOF)) return TriState.TRUE;
+			return TriState.DEFAULT;
 		});
 	}
 
